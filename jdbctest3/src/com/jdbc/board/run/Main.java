67c93ac921286af720d709288a64ac3593c45c4e
GDJ10 <GDJ10@DESKTOP-RO316NV>
2023-04-20 15:07:39 +0900

    02_bs main에 출력문 추가

--- jdbctest3/src/com/jdbc/board/run/Main.java
+++ jdbctest3/src/com/jdbc/board/run/Main.java
@@ -4,8 +4,8 @@ import com.jdbc.board.controller.BoardController;
 
 public class Main {
 	public static void main(String[] args) {
-	
 		new BoardController().mainMenu();
+		System.out.println("내가 추가한것");
 	}
 
 	
